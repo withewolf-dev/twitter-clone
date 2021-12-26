1797f98272eeee555e181c569b22bf4e21d2bdd7
withewolf-dev <itskashyap26@gmail.com>
2021-12-26 19:46:53 +0530

    next.config.js

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,9 +1,16 @@
+import Image from "next/image";
 import React from "react";
 
 interface Props {}
 
 const SideBar = (props: Props) => {
-  return <div></div>;
+  return (
+    <div className="hidden sm:flex flex-col items-center xl:items-start w-[340px] p-2 fixed h-full">
+      <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24">
+        <Image src="https://rb.gy/ogau5a" width={30} height={30} />
+      </div>
+    </div>
+  );
 };
 
 export default SideBar;
